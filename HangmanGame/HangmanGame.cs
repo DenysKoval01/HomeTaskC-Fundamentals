f3a29b69a6fd9da5f004a4477f0f494db3214dd5
Denys Koval <denys_koval@epam.com>
2023-05-03 23:19:54 +0300

    Add project files.

---
+++ HangmanGame/HangmanGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+List<string> words = new() { "cat", "dog", "human", "car", "house" };
+string wordToGuess = words[new Random().Next(0, words.Count)];
+char[] guessedLetters = new char[wordToGuess.Length];
+int lives = 6;
+Console.WriteLine("Guess the word!");
+Console.WriteLine(string.Join(" ", Enumerable.Repeat("_", wordToGuess.Length)));
+while (lives > 0)
+{
+    Console.Write("Enter a letter: ");
+    string input = Console.ReadLine().ToLower();
+    if (input.Length != 1 || !char.IsLetter(input[0]))
+    {
+        Console.WriteLine("Please enter a single letter.");
+        continue;
+    }
+    char guess = input[0];
+    bool correctGuess = false;
+    for (int i = 0; i < wordToGuess.Length; i++)
+    {
+        if (wordToGuess[i] == guess)
+        {
+            guessedLetters[i] = guess;
+            correctGuess = true;
+            break;
+        }
+    }
+    if (!correctGuess)
+    {
+        lives--;
+        Console.WriteLine("No such letter. Lives left: " + lives);
+    }
+    Console.WriteLine(string.Join(" ", guessedLetters.Select(c => c == '\0' ? "_" : c.ToString())));
+    if (!guessedLetters.Contains('\0'))
+    {
+        Console.WriteLine("Congratulations! You guessed the word.");
+        return;
+    }
+}
+Console.WriteLine("Game over. The word was " + wordToGuess);
---
+++ HomeTaskC#Fundamentals/GuessingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+bool playAgain = true;
+
+while (playAgain)
+{
+    int randomNumber = new Random().Next(51);
+
+    int guess = -1;
+
+    while (guess != randomNumber)
+    {
+        Console.Write("I'm thinking of a number between 0 and 50. Can you guess it? ");
+        guess = Convert.ToInt32(Console.ReadLine());
+
+       if (guess < randomNumber)
+           Console.WriteLine("My number is bigger..");
+       else if (guess > randomNumber)
+           Console.WriteLine("My number is smaller..");
+       else
+           Console.WriteLine("Bullseye!");
+    }
+
+    Console.Write("Do you want to play again? (y/n): ");
+    playAgain = Console.ReadLine().ToLower() == "y";
+}
